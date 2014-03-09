c9578e6260d6dbbe30db5d35d9a77cb8220a19a5
Thanasis Polychronakis <thanpolas@gmail.com>
2014-03-09 04:07:09 +0200

    big bang

---
+++ README.md
@@ -0,0 +1,25 @@
+# Idify
+
+Get any unique Id format guaranteed, power by the powerfull Redis Set Data Store.
+
+[![Build Status](https://secure.travis-ci.org/thanpolas/idify.png?branch=master)](http://travis-ci.org/thanpolas/idify)
+
+## Install
+
+Install the module with: `npm install idify --save`
+
+## Documentation
+
+
+
+
+## Release History
+
+- **v0.0.1**, *09 Mar 2014*
+    - Big Bang
+
+## License
+Copyright (c) 2014 Thanasis Polychronakis. Licensed under the MIT license.
+
+[express]: expressjs.com
+[kansas]: https://github.com/thanpolas/kansas
---
+++ test/lib/tester.js
@@ -0,0 +1,28 @@
+/*
+ * @fileOverview Main testing helper lib.
+ */
+
+var tester = module.exports = {};
+
+tester.setup = null;
+tester.teardown = null;
+
+if (global.setup) {
+  tester.setup = setup;
+  tester.teardown = teardown;
+} else {
+  tester.setup = beforeEach;
+  tester.teardown = afterEach;
+}
+
+/**
+ * Have a Cooldown period between tests.
+ *
+ * @param {number} seconds cooldown in seconds.
+ * @return {Function} use is beforeEach().
+ */
+tester.cooldown = function(seconds) {
+  return function(done) {
+    setTimeout(done, seconds);
+  };
+};
